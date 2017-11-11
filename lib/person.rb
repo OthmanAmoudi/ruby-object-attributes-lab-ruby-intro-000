abf0a3898c9e0c47e1c6951bbc25ee04c334198e
Othman AMD <red.fantastic@hotmail.com>
2017-11-11 11:06:43 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,19 +1,19 @@
 class Person
 
-  def name 
-    @name 
+  def name
+    @name
   end
 
   def name=(new_name)
     @name = new_name
   end
 
-  def job 
+  def job
     @job 
   end
 
   def job=(new_job)
     @job = new_job
   end
-  
+
 end
