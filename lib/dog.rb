4f5c69c8288a0dce127ba2ed8a22a6cb826f0d81
Othman AMD <red.fantastic@hotmail.com>
2017-11-11 10:54:46 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  def name 
+    @name = name 
+  end
+
+  def name=(new_name)
+    new_name = @name
+  end
+
+  def breed 
+    @breed = breed
+  end
+
+  def breed=(new_breed)
+    new_breed = @breed
+  end
+end
